81b653ba082cbb495ff74108389a37520305523d
pminhyung <jasonpark9312@gmail.com>
2021-04-08 01:00:37 +0900

    Feat news_scraping function

--- scraping.py
+++ scraping.py
@@ -6,19 +6,23 @@ import openpyxl
 import requests
 import time
 
-def wadiz_scraping(fname = 'wadiz.xlsx'):
-    # 엑셀 파일 만들기
+def scrap_wadiz(fname = 'wadiz.xlsx'):
+    """ scrap project info in Wadiz
+
+    Keyword arguments:
+    fname -- file name to save (xlsx)
+
+    return : saved filename (.xlsx)
+    """
     wb = openpyxl.Workbook()
     sheet = wb.active
 
-    # 맨 윗줄에 컬럼명 추가
     sheet.append(['url', '제목', '카테고리', '메이커', '달성률', '달성액', '서포터수', '좋아요수', '요약글', '목표금액과기간', '글업데이트수', '댓글수', \
                   '리워드종류수', '이미지수', '비디오수', '배송시작날짜', '인스타팔로워수', '와디즈팔로워수', '과거프로젝트수', '과거성공프로젝트수'])
 
-    # 크롬드라이버 실행
     driver = webdriver.Chrome('./chromedriver')
 
-    # 로그인
+    # login
     login_link = 'https://www.wadiz.kr/web/waccount/wAccountLogin?returnUrl=https://www.wadiz.kr/web/main'
     driver.get(login_link)
     id = driver.find_element_by_css_selector('input#userName')
@@ -30,7 +34,7 @@ def wadiz_scraping(fname = 'wadiz.xlsx'):
 
     time.sleep(1.5)
 
-    # 전체 프로젝트 목록 페이지로 이동 (종료된 프로젝트들 마감임박순으로 나열한 페이지)
+    # project lists (recent -> old)
     base_link = 'https://www.wadiz.kr/web/wreward/category/308?keyword=&endYn=Y&order=closing'
     driver.get(base_link)
     driver.maximize_window()
@@ -38,7 +42,7 @@ def wadiz_scraping(fname = 'wadiz.xlsx'):
     time.sleep(1)
 
 
-    # 맨 아래 스크롤
+    # scroll down
     while True:
         try:
             target = driver.find_element_by_css_selector('button.ProjectListMoreButton_button__27eTb')
@@ -50,7 +54,7 @@ def wadiz_scraping(fname = 'wadiz.xlsx'):
             break
 
 
-    # 개별 프로젝트 컨테이너
+    # container of projects
     projects = driver.find_elements_by_css_selector('div.ProjectCardList_item__1owJa')
 
     count = 1
@@ -90,67 +94,67 @@ def wadiz_scraping(fname = 'wadiz.xlsx'):
         print(name)
 
 
-        # 새로운 브라우저 탭에서 프로젝트 세부 페이지 이동
+        # project main page (new tab)
         project_url = image.get_attribute('href')
 
         driver.execute_script("window.open('');")
         time.sleep(1.5)
 
 
-        # 크롬 드라이버의 포커스 이동 - 새로운 탭(세부 페이지)
+        # move focus in chrome (new tab)
         driver.switch_to.window(driver.window_handles[1])
         driver.get(project_url)
         time.sleep(1.5)
 
-        # 서포터 수
+        # supporters-num
         try:
             supporters = driver.find_element_by_css_selector('p.total-supporter strong').text
         except:
             supporters = 0
 
-        # 좋아요(하트) 수
+        # likes
         try:
             likes = driver.find_element_by_css_selector('em.cnt-like').text
         except:
             likes = 0
 
-        # 요약글 텍스트
+        # summary text
         try:
             summary = driver.find_element_by_css_selector('div.campaign-summary').text
         except:
             summary = 'None'
 
-        # 프로젝트 목표 금액과 기간
+        # amount of funding and period
         try:
             goal_amount = driver.find_element_by_css_selector('div.wd-ui-campaign-content > div > div:nth-child(4) p').text
         except:
             goal_amount = 'no info'
 
-        # 새로운 글 업데이트 수
+        # updated-postings-num
         try:
             new_news = driver.find_element_by_css_selector('ul.tab-list li:nth-of-type(4) span').text
         except:
             new_news = 0
 
-        # 댓글 수(커뮤니티 수)
+        # comments-num (community-posting-nums)
         try:
             comment_num = driver.find_element_by_css_selector('ul.tab-list li:nth-of-type(5) span').text
         except:
             comment_num = 0
 
-        # 리워드 종류 수
+        # rewards-num
         try:
             reward_num = len(driver.find_elements_by_css_selector('button.rightinfo-reward-list'))
         except:
             reward_num = 0
 
-        # 소개글 이미지 수
+        # intro image-num
         try:
             img_num = len(driver.find_elements_by_css_selector('div.inner-contents.fr-view img'))
         except:
             img_num = 0
 
-        # 소개글 비디오 수
+        # intro video-num
         try:
             video_num = len(driver.find_elements_by_css_selector('span.fr-video.fr-fvc.fr-dvb.fr-draggable'))
             print(video_num)
@@ -160,23 +164,23 @@ def wadiz_scraping(fname = 'wadiz.xlsx'):
 
 
 
-        # 펀딩 안내페이지 이동
+        # main page of funding
         funding_info_btn = driver.find_element_by_css_selector('ul.tab-list li:nth-of-type(3) a')
         funding_info_btn.click()
 
-        # 배송 시작 날짜
+        # start date of delievery
         try:
             delivery_date = driver.find_element_by_css_selector('div#detail-funding-info div.content h3 em').text
         except:
             deliver_date = 'no info'
 
-        # 커뮤니티 페이지 들어가기
+        # community
         community_btn = driver.find_element_by_css_selector('ul.tab-list li:nth-of-type(5) a')
         community_btn.click()
 
-        # 댓글 - 수집X
+        # comments (X)
 
-        # 새로운 탭에서 인스타그램 이동 후 팔로워수 수집
+        # Instagram (new tab)
         try:
             instagram = driver.find_element_by_css_selector('ul.social a.instagram')
             instagram_url = instagram.get_attribute('href')
@@ -188,10 +192,10 @@ def wadiz_scraping(fname = 'wadiz.xlsx'):
             driver.get(instagram_url)
             time.sleep(1.5)
             try:
-                # 인스타 팔로워 수
+                # follower-num (Instagram)
                 sns_followers = driver.find_element_by_css_selector('ul.k9GMp  li:nth-of-type(2)  span.g47SY').text
             except:
-                # 링크에 에러가 있거나 계정이 삭제된 경우
+                # profile deleted, link error
                 sns_followers = 'link error'
 
             driver.close()
@@ -204,25 +208,25 @@ def wadiz_scraping(fname = 'wadiz.xlsx'):
         time.sleep(1.5)
 
 
-        # 새로운 탭에서 메이커 프로필 페이지 이동
+        # maker profile page (new tab)
         maker_profile = driver.find_element_by_css_selector('div.maker-info button')
         maker_profile.click()
 
         time.sleep(1.5)
 
-        # 와디즈 팔로워 수
+        # follower-num in wadiz
         try:
             wadiz_followers = driver.find_element_by_css_selector('ul.activity-list li:nth-of-type(3) strong').text
         except:
             wadiz_followers = 0
 
-        # 과거 리워드 프로젝트 수
+        # reward project-num in the past
         try:
             past_projects_num = len(driver.find_elements_by_css_selector('li.all em.project-type.reward'))-1
         except:
             past_projects_num = 0
 
-        # 과거 성공한 프로젝트 수(현재 포함)
+        # succeeded project-num so far
         try:
             past_projects = driver.find_elements_by_css_selector('li.all span.percent')
             n = 0
@@ -234,17 +238,16 @@ def wadiz_scraping(fname = 'wadiz.xlsx'):
             past_success_projects_num = 0
 
 
-        # 시트에 데이터 저장
         sheet.append([project_url, name, category, maker, percent, money, supporters, likes, summary, goal_amount, new_news, comment_num,\
                       reward_num, img_num, video_num, delivery_date, sns_followers, wadiz_followers, past_projects_num, \
                       past_success_projects_num])
 
 
-        # 세부 페이지 탭 닫기
+        # close detail-page tab
         driver.close()
         time.sleep(1.5)
 
-        # 원래 탭(전체 목록 페이지)으로 이동
+        # back to initial tab
         driver.switch_to.window(driver.window_handles[0])
 
         count += 1
@@ -253,11 +256,79 @@ def wadiz_scraping(fname = 'wadiz.xlsx'):
         wb.save(fileName)
         idx += 1
 
-    # 크롬드라이버 종료
     driver.close()
 
-    print('수집 종료')
+    print('finish scraping')
 
-    # 엑셀 파일 저장
+    wb.save(fname)
+    return fname
+
+def scrap_navernews(keyword: str = '크라우드펀딩'):
+    """ scrap naver news with specific keyword
+
+    Keyword arguments:
+    keyword -- search keyword in naver news
+
+    return : saved filename (.xlsx)
+    """
+
+    wb = openpyxl.Workbook()
+    sheet = wb.active
+
+    sheet.append(['키워드', '날짜', '기사제목'])
+
+    keyword = '크라우드펀딩'
+
+    # headless chromedriver
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+
+    driver = webdriver.Chrome('./chromedriver', options=options)
+
+    # max_num of articles scrapped
+    max = 3999
+
+    def check_exists_by_css_selector(css_selector):
+        try:
+            driver.find_element_by_css_selector(css_selector)
+        except:
+            return False
+        return True
+
+    for n in range(1, max, 10):
+        driver.get('https://search.naver.com/search.naver?&where=news&query=크라우드펀딩&sm=tab_pge&sort=1&photo=0&field=0&reporter_article=&pd=3&ds=2019.06.01&de=2019.08.14&docid=&nso=so:dd,p:from20190601to20190814,a:all&mynews=0&start='+str(n)+'&refresh_start=0')
+        articles = driver.find_elements_by_css_selector('ul.type01>li')
+
+        for a in articles:
+            # title
+            title = a.find_element_by_css_selector("a._sp_each_title").text
+            # date
+            date = a.find_element_by_css_selector("dd.txt_inline").text
+
+            print(title)
+            sheet.append([keyword, date, title])
+
+            # similar articles
+            if check_exists_by_css_selector('ul.type01>li dl dd:nth-of-type(3)') == True:
+                # see more button
+                try:
+                    more_button = a.find_element_by_css_selector('ul.type01>li div.newr_more').text
+                    num_of_more_articles = re.search(r'(\d+)', more_button)
+                    for _ in range(int(num_of_more_articles.group(1))):
+                        sheet.append([keyword, date, '[연관기사]'+title])
+                        print('similar articles')
+
+                # few similar articles
+                except:
+                    related_articles = a.find_elements_by_css_selector('ul.type01>li ul.relation_lst li')
+                    for r in related_articles:
+                        related_articles_title = r.find_element_by_css_selector('ul.type01>li ul.relation_lst li a').text
+                        related_articles_date = r.find_element_by_css_selector('ul.type01>li ul.relation_lst li span.txt_sinfo').text
+
+                        print(related_articles_title)
+                        sheet.append([keyword, related_articles_date, related_articles_title])
+
+    print('finish scrapping!')
+    fname = "navernews_{}.xlsx".format(keyword)
     wb.save(fname)
     return fname
