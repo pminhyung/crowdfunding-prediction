f48874161569e77a6b7f19f97111b81f09625b07
pminhyung <jasonpark9312@gmail.com>
2021-04-08 12:28:19 +0900

    Feat save_model
    This saves pretrained model in "pretrained_models" directory

--- xgb_train.py
+++ xgb_train.py
@@ -1,3 +1,6 @@
+import os
+import pickle
+
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 import sklearn.metrics as mt
@@ -9,26 +12,23 @@ from scraping import scrap_wadiz, scrap_navernews
 from utils import get_cat_success_rate
 from preprocessing import preprocess_data
 
+SAVE_MODEL = True
+
 wadiz_fname = scrap_wadiz('wadiz.xlsx')
 #naver_fname = scrap_navernews('크라우드펀딩')
 
+# load data
 wadiz = pd.read_excel(wadiz_fname, engine = 'openpyxl')
 #naver = pd.read_excel(naver_fname, engine = 'openpyxl')
 
-df = preprocess_data(wadiz)
+# preprocess
+prep_data, X, y = preprocess_data(wadiz)
 
-
-X = df.drop(['log_count'], axis=1)
-y = df['log_count']
-
-# 학습용/테스트용 데이터 분리
-
-
+# train validation set split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100)
 
 
-# Random Forest Regressor
-
+### Random Forest Regressor
 # rf = RandomForestRegressor(n_estimators = 1000)
 #
 # rf.fit(X_train, y_train)
@@ -38,7 +38,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 #
 # y_pred = rf.predict(X_test)
 #
-# #평가 지표 값들을 계산한다.
 # mse = mt.mean_squared_error(y_test, y_pred)
 # rmse = np.sqrt(mse)
 # r2 = mt.r2_score(y_test, y_pred)
@@ -49,22 +48,19 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 # feat_imp = pd.DataFrame({'속성명':X.columns, '중요도':rf.feature_importances_}).sort_values('중요도',ascending=False)
 # print(feat_imp)
 
-# log transformation한 달성률(log_count)을 예측
+# log 달성률(log_count) 예측
 # y_pred = rf.predict(X_test)
 # # log transformation한 달성률(log_count)을 다시 exp로 원상복귀
 # predictions = np.exp(y_pred) - 1
 
-## XGBoost - Classification
+## XGBoost train
 xgb_clf = XGBClassifier()
 xgb_clf.fit(X_train, y_train)
 
 xgb_acc = xgb_clf.score(X_test, y_test)
 print(xgb_acc)
 
-# gridsearch
-
-
-
+# GridsearchCV
 xgb_params = {'n_estimators':[100,200],
              'learning_rate':[0.1,0.2]}
 
@@ -76,3 +72,12 @@ best_params = grid_xgb.best_params_
 
 print(grid_xgb)
 print(best_params)
+
+# save model
+if SAVE_MODEL:
+    path = "pretrained_models"
+
+    if not os.path.isdir(path):
+        os.mkdir(path)
+
+    pickle.dump(grid_xgb, open("pretrained_models/xgb.dat", "wb"))
